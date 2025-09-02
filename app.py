fe55fc99f89555553e0bdf501b9ec949b2428119
sammyyo <erhini@yahoo.com>
2025-09-02 18:14:43 +0200

    first commit

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
-
-allow_origins=[
+    allow_origins=[
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://qandro.com/",
-]
+],
+)
+
+
 
 
 class SumReq(BaseModel):
